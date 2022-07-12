ab1ef4603c568b812a64962ec1d918bc53c33724
Abdelrhman97Shaban <abdelrhmanshaban697@gmail.com>
2022-07-12 17:28:36 +0200

    Add assignment of struct

--- First_Term/Unit_2_C_Programming/5_Structure/EX_3.c
+++ First_Term/Unit_2_C_Programming/5_Structure/EX_3.c
@@ -1,1 +1,25 @@
-#include 
+#include <stdio.h> 
+
+typedef struct 
+{
+    float img; 
+    float real; 
+}Scomplex_t;
+
+Scomplex_t C_num1, C_num2, sum;  
+
+int main (void)
+{
+    printf("For 1st num Enter real and img respectively: "); 
+    scanf("%f",&C_num1.real); 
+    scanf("%f",&C_num1.img);
+    printf("For 2nd num Enter real and img respectively: "); 
+    scanf("%f",&C_num2.real); 
+    scanf("%f",&C_num2.img);
+    
+    sum.real = C_num1.real + C_num2.real; 
+    sum.img = C_num1.img + C_num2.img;
+
+    printf("Sum = %.1f + %0.1fi",sum.real,sum.img);  
+    return 0; 
+}
---
+++ First_Term/Unit_2_C_Programming/5_Structure/EX_4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+typedef struct 
+{
+    char name [50];
+    int roll; 
+    float mark; 
+}Sstudent_t;
+
+
+int main (void)
+{   
+    Sstudent_t students[10]; 
+    int i; 
+    printf("Enter information of students: \n");
+    for (i = 0; i < 10; i++)
+    {
+        students[i].roll = i + 1; 
+        printf("for roll number %d\n", students[i].roll);
+        printf("Enter Name: "); 
+        scanf("%s",&students[i].name); 
+        printf("Enter marks: ");
+        scanf("%f",&students[i].mark);
+    }
+
+    printf("\nDisplaying Information: \n");
+    for (i = 0; i < 10; i++)
+    {
+        printf("Information for number %d:\n",students[i].roll);
+        printf("Name: %s\n",students[i].name);
+        printf("Name: %0.2f\n",students[i].mark);
+    }
+  
+   
+    return 0; 
+}
